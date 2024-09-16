d9bf98d01df3d54dcea5f3301d64f769fccb14a3
Zachary Caterer <catererzach@yahoo.com>
2024-09-16 16:01:29 -0600

    updated pytest script to contain the object rather than functions

--- test/seq_test.py
+++ test/seq_test.py
@@ -1,16 +1,25 @@
 import pytest
-from SOM_Seq_Sim.Seq_Sim.utils import *
+import pandas as pd
+from SOM_Seq_Sim.Seq_Sim.utils import DataPlotter
 
 
 def test_optimal_layout():
-    assert optimal_subplot_layout(9) == (3, 3)
-    assert optimal_subplot_layout(10) == (2, 5)
-    assert optimal_subplot_layout(11) == (6, 2)
-    assert optimal_subplot_layout(12) == (2, 6)
+    # Create an instance of DataPlotter with dummy data
+    dummy_metadata = pd.DataFrame()
+    plotter = DataPlotter(metadata=dummy_metadata)
+
+    assert plotter.optimal_subplot_layout(9) == (3, 3)
+    assert plotter.optimal_subplot_layout(10) == (2, 5)
+    assert plotter.optimal_subplot_layout(11) == (6, 2)
+    assert plotter.optimal_subplot_layout(12) == (2, 6)
 
 
 def test_remainder():
-    assert remainder(10, 3) == 1
-    assert remainder(12, 5) == 2
-    assert remainder(7, 2) == 1
-    assert remainder(6, 3) == 0
+    # Create an instance of DataPlotter with dummy data
+    dummy_metadata = pd.DataFrame()
+    plotter = DataPlotter(metadata=dummy_metadata)
+
+    assert plotter.remainder(10, 3) == 1
+    assert plotter.remainder(12, 5) == 2
+    assert plotter.remainder(7, 2) == 1
+    assert plotter.remainder(6, 3) == 0
