8c8574b31e317eae5d1a611fdfab4374c440e69d
madrpernat <madeline.pernat@colorado.edu>
2024-10-08 14:16:56 -0600

    component plane plotting

--- Seq_Sim/examples/som_example.py
+++ Seq_Sim/examples/som_example.py
@@ -1,8 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from Seq_Sim.utils.SOM import SOM
 
+# Training data: Only considering two features here. This data has already undergone PCA and so 
+# there aren't really any patterns between the features. This is more just a proof of concept
+# that the training and plotting works.
 train_dat = pd.read_csv("./data/simulation/simulated_pc_NAM_HARMONIZED_jade_woi_advanced_numCell100_numSamp10_sd0.1_fci0.1_clusterRatio0.7_diseaseRatio0.05.csv")
+train_dat = train_dat.iloc[:, 0:2] 
+
+# Other data: Data that is NOT used for training but still could be interesting to project onto the
+# map. No plotting methods for this yet.
 other_dat = pd.read_csv("./data/simulation/simulated_umap_data_jade_woi_advanced_numCell100_numSamp10_sd0.1_fci0.1_clusterRatio0.7_diseaseRatio0.05.csv")
 
 som = SOM(
@@ -13,12 +19,11 @@ som = SOM(
     y_dim=6,
     topology="hexagonal",
     neighborhood_fnc="gaussian",
-    epochs=2
+    epochs=5
 )
 
 som.train_map()
 
-som.plot_map_grid(print_neuron_idx=True)
-plt.show()
-
-print("trained")
+som.plot_component_planes(
+    output_dir="Seq_Sim/examples/output_figs"
+)
--- Seq_Sim/utils/SOM.py
+++ Seq_Sim/utils/SOM.py
@@ -1,46 +1,57 @@
-from typing import Union
+import os
+from typing import Tuple, Union
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.colors as mcolors
+import matplotlib.figure
 from minisom import MiniSom
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
 
 class SOM():
     def __init__(
-            self,
-            train_dat: Union[pd.DataFrame, np.ndarray],
-            other_dat: Union[pd.DataFrame, np.ndarray],
-            scale_method: str,
-            x_dim: int,
-            y_dim: int,
-            topology: str,
-            neighborhood_fnc: str,
-            epochs: int
+        self,
+        train_dat: Union[pd.DataFrame, np.ndarray],
+        other_dat: Union[pd.DataFrame, np.ndarray],
+        scale_method: str,
+        x_dim: int,
+        y_dim: int,
+        topology: str,
+        neighborhood_fnc: str,
+        epochs: int
     ):
-        self.train_dat = train_dat if isinstance(train_dat, np.ndarray) else train_dat.to_numpy()
-        self.other_dat = other_dat if isinstance(other_dat, np.ndarray) else other_dat.to_numpy()
-        self._scale = getattr(self, f"{scale_method}_scale", None)
-        self._unscale = getattr(self, f"{scale_method}_unscale", None)
+        self.train_dat = train_dat.to_numpy() if isinstance(train_dat, pd.DataFrame) else train_dat
+        self.other_dat = other_dat.to_numpy() if isinstance(other_dat, pd.DataFrame) else other_dat
+
+        # Scaling and unscaling methods
+        self._scale = getattr(self, f"_{scale_method}_scale", None)
+        self._unscale = getattr(self, f"_{scale_method}_unscale", None)
+
         self.train_dat_scaled, self._scaling_factors = self._scale()
-        self.x_dim = x_dim
+        self.xdim = x_dim
         self.ydim = y_dim
         self.topology = topology
         self.neighborhood_fnc = neighborhood_fnc
         self.epochs = epochs
+
+        # Attributes to be set later
         self.map = None
-        self.neuron_idx = None
+        self.observation_mapping = None
         self.neuron_coordinates = None
         self.weights = None
         self.weights_scaled = None
 
     def train_map(self):
-        train_dat_scaled = self.train_dat_scaled
-        n_samples = train_dat_scaled.shape[0]
-        n_features = train_dat_scaled.shape[1]
+        """_
+        Train the SOM and extract neuron coordinates, weights, and observation mappings
+        """
+        n_samples = self.train_dat_scaled.shape[0]
+        n_features = self.train_dat_scaled.shape[1]
 
         som = MiniSom(
-            x=self.x_dim,
+            x=self.xdim,
             y=self.ydim,
             input_len=n_features,
             sigma=1,
@@ -50,94 +61,156 @@ class SOM():
             activation_distance='euclidean',
             random_seed=0
         )
+        som.pca_weights_init(self.train_dat_scaled)
         som.train(
-            data=train_dat_scaled,
+            data=self.train_dat_scaled,
             num_iteration=self.epochs * n_samples,
             random_order=True,
             verbose=False
         )
-        # Map each observation to neuron
-        winner_coordinates = np.array([som.winner(x) for x in train_dat_scaled]).T
-        neuron_idx = np.ravel_multi_index(winner_coordinates, (self.x_dim, self.ydim))
 
-        # Get coordiantes of each neuron
-        xx, yy = som.get_euclidean_coordinates()
-        weights = som.get_weights()
-
-        x_coords = [xx[(i, j)] for i in range(weights.shape[0]) for j in range(weights.shape[1])]
-        y_coords = [yy[(i, j)] for i in range(weights.shape[0]) for j in range(weights.shape[1])]
-        neuron_coordiantes = pd.DataFrame({'x': x_coords, 'y': y_coords})
-
-        # Get weights of each neuron
-        weights_scaled = []  # "scaled" since trained on scaled data
-        for i in range(self.x_dim * self.ydim):
-            neuron_idx = np.unravel_index(i, (self.x_dim, self.ydim))
-            row, col = neuron_idx[0], neuron_idx[1]
-            weight_scaled = som.get_weights()[row, col, :]
-            weights_scaled.append(weight_scaled)
-        weights_scaled = pd.DataFrame(weights_scaled)
-
-        # Set attributes
+        # Set attributes yielded from training
         self.map = som
-        self.train_dat_scaled = train_dat_scaled
-        self.neuron_idx = neuron_idx
-        self.neuron_coordinates = neuron_coordiantes
-        self.weights_scaled = weights_scaled
-        self.weights = self._unscale(weights_scaled)
+        self.observation_mapping = self._get_observation_neuron_mappings()
+        self.neuron_coordinates = self._get_neuron_coordinates()
+        self.weights_scaled = self._get_weights()
+        self.weights = self._unscale(self.weights_scaled)
 
-    def zscore_scale(self):
+    def _zscore_scale(self):
         means = np.mean(self.train_dat, axis=0)
         stds = np.std(self.train_dat, axis=0)
         return (self.train_dat - means) / stds, [means, stds]
 
-    def minmax_scale(self):
+    def _minmax_scale(self):
         min_vals = np.min(self.train_dat, axis=0)
         max_vals = np.max(self.train_dat, axis=0)
         return (self.train_dat - min_vals) / (max_vals - min_vals), [min_vals, max_vals]
 
-    def zscore_unscale(self, scaled_data):
+    def _zscore_unscale(self, scaled_data):
         means, stds = self._scaling_factors
         return scaled_data * stds + means
 
-    def minmax_unscale(self, scaled_data):
+    def _minmax_unscale(self, scaled_data):
         min_vals, max_vals = self._scaling_factors
         return scaled_data * (max_vals - min_vals) + min_vals
 
-    def plot_component_planes(self):
-        fig, ax = self.plot_map_grid()
+    def _get_observation_neuron_mappings(self):
+        """
+        Get the neuron id that each observation in the training data is assigned to
+        """
+        # Get the x-y coordiantes of the winning neurons
+        winner_coordinates = np.array([
+            self.map.winner(x) for x in self.train_dat_scaled
+        ]).T
 
-        for 
+        # Convert x-y coordinate to "neuron id"
+        winner_neuron = np.ravel_multi_index(
+            multi_index=winner_coordinates,
+            dims=(self.xdim, self.ydim)
+        )
+        return winner_neuron
 
-        return
+    def _get_neuron_coordinates(self):
+        """
+        Get the x-y coordinates of the SOM grid's neurons
+        """
+        HEX_CORRECTION = np.sqrt(3)/2
 
-    def plot_categories(self):
-        return
+        xx, yy = self.map.get_euclidean_coordinates()
 
-    def plot_map_grid(self, print_neuron_idx=False):
+        x_coords = [xx[(i, j)] for i in range(self.xdim) for j in range(self.ydim)]
+        if self.topology == 'hexagonal':
+            y_coords = [
+                yy[(i, j)] * HEX_CORRECTION for i in range(self.xdim) for j in range(self.ydim)
+            ]
+        else:
+            y_coords = [yy[(i, j)] for i in range(self.xdim) for j in range(self.ydim)]
 
-        BASE_SIZE_PER_NEURON = 1
+        return pd.DataFrame({
+            'x': x_coords,
+            'y': y_coords
+        })
 
-        # Calculate fig size based on SOM dimensions
-        fig_width = BASE_SIZE_PER_NEURON * self.x_dim
-        fig_height = BASE_SIZE_PER_NEURON * self.ydim
+    def _get_weights(self):
+        """
+        Get the weights of each neuron after training on scaled data.
+        """
+        # weights_scaled = []  # "scaled" since trained on scaled data
+        # for i in range(self.x_dim * self.ydim):
+        #     neuron_idx = np.unravel_index(i, (self.x_dim, self.ydim))
+        #     row, col = neuron_idx[0], neuron_idx[1]
+        #     weight_scaled = som.get_weights()[row, col, :]
+        #     weights_scaled.append(weight_scaled)
+        # weights_scaled = pd.DataFrame(weights_scaled)
 
-        # Calculate radius of circles (neurons) for plotting
-        circle_radius = 0.5
+        weights_scaled = [
+            self.map.get_weights()[i, j, :] for i in range(self.xdim) for j in range(self.ydim)
+        ]
+        return pd.DataFrame(weights_scaled)
 
-        fig, ax = plt.subplots(figsize=(fig_width, fig_height))
+    def plot_component_planes(
+        self,
+        output_dir: str
+    ):
+        """
+        Plot component planes for each feature in the training data and save the figures.
+        """
+        # Color scheme
+        cmap = mcolors.LinearSegmentedColormap.from_list(
+            name='cmap',
+            colors=[(0, '#008000'), (0.5, '#FFFF00'), (1, '#FF0000')]
+        )
+
+        # Create plot for each feature
+        for feature_idx in range(self.train_dat.shape[1]):
+            fig, ax = self.plot_map_grid()
+            feature_weights = self.weights.iloc[:, feature_idx]
+            feature_weights_range = (np.min(feature_weights), np.max(feature_weights))
+
+            # Fill grid with neuron values
+            for neuron_idx, row in self.neuron_coordinates.iterrows():
+                neuron_value = feature_weights[neuron_idx]
+                neuron_fill_color = SOM._map_value_to_color(
+                    value=neuron_value,
+                    value_range=feature_weights_range,
+                    cmap=cmap
+                )
+                SOM._draw_circle(
+                    ax=ax,
+                    x=row['x'],
+                    y=row['y'],
+                    fill_color=neuron_fill_color,
+                    edge_color='none'
+                )
+       
+            SOM._add_colorbar(
+                fig=fig,
+                value_range=feature_weights_range,
+                cmap=cmap
+            )
+            fig.suptitle(f"Feature {feature_idx}", y=0.95)
+            plt.savefig(os.path.join(output_dir, f"feature{feature_idx}_component_plane.png"))
+
+    def plot_map_grid(
+        self, 
+        print_neuron_idx: bool = False
+    ) -> Tuple[plt.Figure, plt.Axes]:
+        """
+        Plot a blank SOM grid with optional neuron ids
+        """
+        fig, ax = plt.subplots(figsize=(self.xdim, self.ydim))
 
         # Plot each neuron as a circle
         for idx, row in self.neuron_coordinates.iterrows():
-            x, y = row['x'], row['y'] * np.sqrt(3) / 2
-
-            circle = patches.Circle(
-                xy=(x, y),
-                radius=circle_radius,
-                edgecolor='black',
-                facecolor='none'
+            x, y = row['x'], row['y']
+            SOM._draw_circle(
+                ax=ax,
+                x=x,
+                y=y,
+                fill_color='none',
+                edge_color='black'
             )
-            ax.add_patch(circle)
-
+            # Show neuron id in each neuron
             if print_neuron_idx:
                 ax.text(
                     x=x,
@@ -150,10 +223,80 @@ class SOM():
                 )
 
         ax.set_aspect('equal')
-        ax.set_xlim([-1.5, self.x_dim])
+        ax.set_xlim([-1.5, self.xdim])
         ax.set_ylim([-1, self.ydim - 0.5])
         ax.axis('off')
-        ax.margins(0)
-        fig.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
+        fig.subplots_adjust(
+            left=0.05,
+            right=0.95,
+            top=0.95,
+            bottom=0.05
+        )
 
         return fig, ax
+
+    @staticmethod
+    def _draw_circle(
+        ax,
+        x: float,
+        y: float,
+        fill_color: Union[Tuple[float, float, float, float], str],
+        edge_color: Union[Tuple[float, float, float, float], str],
+        radius: float = 0.5
+    ):
+        """
+        Draw a circle on the provided ax object at specified coordinates
+        """
+        circle = patches.Circle(
+                    xy=(x, y),
+                    radius=radius,
+                    facecolor=fill_color,
+                    edgecolor=edge_color
+                )
+        ax.add_patch(circle)
+
+    @staticmethod
+    def _map_value_to_color(
+        value: float,
+        value_range: Tuple[float, float],
+        cmap: mcolors.LinearSegmentedColormap
+    ) -> Tuple[float, float, float, float]:
+
+        # Normalize the value to the range [0, 1]
+        normalized_value = (value - value_range[0]) / (value_range[1] - value_range[0])
+
+        # Map the normalized value to a color in the gradient
+        return cmap(normalized_value)
+
+    @staticmethod
+    def _add_colorbar(
+        fig: matplotlib.figure.Figure,
+        value_range: Tuple[float, float],
+        cmap: mcolors.LinearSegmentedColormap
+    ):
+        """
+        Add colorbar to component plane figures
+        """
+        norm = plt.Normalize(
+            vmax=value_range[1],
+            vmin=value_range[0]
+        )
+        sm = plt.cm.ScalarMappable(
+            cmap=cmap,
+            norm=norm
+        )
+        sm.set_array([])
+
+        fig.subplots_adjust(bottom=0.1)
+        cbar_ax = fig.add_axes([0.125, 0.1, 0.775, 0.03])
+        cbar = fig.colorbar(
+            mappable=sm,
+            cax=cbar_ax,
+            orientation='horizontal'
+        )
+        cbar.set_label(
+            label='Color: Neuron Feature Value',
+            fontsize=15,
+            labelpad=10
+        )
+        cbar.ax.tick_params(labelsize=14)
